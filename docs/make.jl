6a0c0732288daeb7b40d02f27db3c93a202d6b81
angkunwu <gilbert.wu14@gmail.com>
2026-02-06 18:20:38 -0500

    update make.jl for dev

--- docs/make.jl
+++ docs/make.jl
@@ -12,4 +12,14 @@ makedocs(
 # Deployment is handled by the GitHub Actions workflow which uploads
 # `docs/build` to GitHub Pages. Avoid calling `deploydocs` here so local
 # builds don't attempt to push.
+if get(ENV, "GITHUB_ACTIONS", "") != ""
+    # When running in GitHub Actions, let Documenter handle deployment so
+    # it creates the versioned `dev/` folder for non-release builds.
+    deploydocs(
+        repo = "github.com/angkunwu/HelloWorld",
+        branch = "gh-pages",
+    )
+else
+    @info "Not running in GitHub Actions - skipping deploydocs() to avoid pushing from local builds"
+end
 
